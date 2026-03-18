63ddbf898ed1eba4427c362409003cfbc2af2690
Liam <0622557@schoolsnet.act.edu.au>
2026-03-18 23:01:23 +1100

    gn

--- time.py
+++ time.py
@@ -22,45 +22,46 @@ while True:
   image = cv.Canny(image, 30,200)
 
   contours, hierarchy = cv.findContours(image,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+  poly = None
+  for cnt in contours:
+    M = cv.moments(cnt)
+    perimeter = cv.arcLength(cnt, True)
+    epsilon = 10 * perimeter
+    poly = cv.approxPolyDP(cnt, epsilon, True)
 
   imageC = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-  filter = [(cv.minAreaRect(c)) for c in contours if cv.contourArea(c) > 100]
+  rects = [cv.minAreaRect(c) for c in poly if cv.contourArea(c) > 50]
 
 
-  mindiff = 360
+ 
+    
+
+  mindiff = 180
   pair = (None, None)
   
-  for i in range(len(filter)):
-    for j in range(i+1, len(filter)):
-      rect1 = filter[i][1]
-      rect2 = filter[j][1]
-
-      linediff = abs(rect1[2] - rect2[2])
+  for i in range(len(rects)):
+    for j in range(i+1, len(rects)):
+      linediff = abs(rects[i][2] - rects[j][2])
       linediff = min(linediff, 180 - linediff)
 
       if linediff < mindiff:
         mindiff = linediff
-        pair = (filter[i][0], filter[j][0])
+        pair = (contours[i], contours[j])
 
   if pair[0] is not None:
-    cv.drawContours(image, pair[0], -1, (255,255,255),7) 
-    cv.drawContours(image, pair[1], -1, (255,255,255),7) 
+    cv.drawContours(image, pair[0], -1, (255,255,255),2) 
+    cv.drawContours(image, pair[1], -1, (255,255,255),2) 
 
   
     for item in pair:
-      rows, cols = imageC.shape[:2]
+      rows, cols = (imageC.shape[:2])
       [vx, vy, x, y,] = cv.fitLine(item, cv.DIST_L2,0,0.01,0,0.01)
-      lefty = int((-x*vy/vx)+y)
-      righty = int(((cols-x)*vy/vx)+y)
-      cv.line(imageC,(cols-1,righty),(0,lefty),(0,255,0),2)
+      if vx > 0:
+        lefty = int((-x*vy/vx)+y)
+        righty = int(((cols-x)*vy/vx)+y)
+        cv.line(imageC,(cols-1,righty),(0,lefty),(0,255,0),2)
 
   cv.imshow('imageC', imageC)
-
-
-
-
-  
-  
 
   key = cv.waitKey(1)
   if key == 27: # exit on ESC
