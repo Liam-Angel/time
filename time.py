28240bee44b43dde8e2e74919bcd1db2eb18b5df
Liam <0622557@schoolsnet.act.edu.au>
2026-04-02 11:48:18 +1100

    ok fine

--- time.py
+++ time.py
@@ -4,13 +4,17 @@ import cv2 as cv
 import os, sys, inspect
 import numpy as np;
 import serial
+import datetime
 import time
 
 cam = cv.VideoCapture(0)
+#prevtime = datetime.datetime.now()
 
 while True:
-  ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+  #nowtime = datetime.datetime.now()
+  ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0)
   ser.reset_input_buffer()  
+  #print(datetime.datetime.now())
 
   check, frame = cam.read()
   img = cv.resize(frame,(320,240))
@@ -18,7 +22,7 @@ while True:
   image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
   image = cv.GaussianBlur(image, (11, 11), sigmaX=0)
 
-  _, image = cv.threshold(image, 150, 255, cv.THRESH_BINARY)
+  _, image = cv.threshold(image, 125, 255, cv.THRESH_BINARY)
 
   image = cv.Canny(image, 60,200)
 
@@ -29,19 +33,6 @@ while True:
   filter = [c for c in contours if (cv.minAreaRect(c)[1][0] + cv.minAreaRect(c)[1][1]) >80]
   rects = [cv.minAreaRect(c) for c in filter]
 
-  for item in rects:
-    point = item[0]
-
-    rot1 = int(item[0][0])
-    rot2 = int(item[0][1])
-
-    word = str(rot1) + ", " + str(rot2)
-
-    box = cv.boxPoints(item)
-    box = np.intp(box)
-    cv.drawContours(imageC,[box],0,(255,255,0),2)
-    cv.putText(imageC, word,(int(point[0]), int(point[1])),cv.FONT_HERSHEY_SIMPLEX,0.5,(0,255,255),1)
-
   mindiff = 180
   pair = (None, None)
   
@@ -49,7 +40,7 @@ while True:
     for j in range(i+1, len(rects)):
 
       ri = rects[i][2]
-      rj = rects[i][2]
+      rj = rects[j][2]
 
       wi, hi = rects[i][1]
       wj, hj = rects[j][1]
@@ -69,14 +60,21 @@ while True:
         recty = int((rects[j][0][1] + rects[i][0][1])/2)
           
   if pair[0] is not None:
-    cv.circle(imageC, (rectx, recty), 20, (255,0,0),2)
+    cv.circle(imageC, (rectx, recty), 10, (255,0,0),2)
 
+    #if (prevtime - time).total_seconds() > 3:
     ser.write(bytes(str(rectx), encoding="utf-8"))
-    print(rectx)
-    time.sleep(1)
-    if ser.in_waiting > 0:          
-      line = ser.readline().decode('utf-8').rstrip()
-      print("line", line)
+
+
+    #print(prevtime)
+    #print((prevtime - time).total_seconds())
+    
+
+    #print(rectx)
+    #time.sleep(0.1)
+    #if ser.in_waiting > 0:          
+      #line = ser.readline().decode('utf-8').rstrip()
+      #print("line", line)
 
     for item in pair:
       rows, cols = (imageC.shape[:2])
@@ -86,6 +84,7 @@ while True:
         lefty = int((-x*vy/vx)+y)
         righty = int(((cols-x)*vy/vx)+y)
         cv.line(imageC,(cols-1,righty),(0,lefty),(0,255,0),2)
+        #print(vx, vy, x, y)
 
   cv.imshow('imageC', imageC)
 
