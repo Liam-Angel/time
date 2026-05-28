27025791562c027afbe45454756bcf28cbd21760
Liam-Angel <0622557@schoolsnet.act.edu.au>
2026-05-28 10:53:51 +1000

    Create recent.py
    new

---
+++ recent.py
@@ -0,0 +1,145 @@
+from PIL import Image
+from datetime import datetime
+import cv2 as cv
+import numpy as np;
+import serial
+
+cam = cv.VideoCapture(0, cv.CAP_V4L2)
+cam.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+cam.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+cam.set(cv.CAP_PROP_FPS, 30)
+
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter('Anh/' + str(datetime.now()) + '.avi', fourcc, 10.0, (640, 480))
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.reset_input_buffer() 
+
+while True: 
+  check, frame = cam.read()
+
+  
+
+  image = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+  image = cv.GaussianBlur(image, (11, 11), sigmaX=0)
+
+  _, image = cv.threshold(image, 90, 255, cv.THRESH_BINARY)
+
+  image = cv.Canny(image, 40,200)
+
+  contours, hierarchy = cv.findContours(image,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+
+  
+
+  filter = [c for c in contours if (cv.minAreaRect(c)[1][0] * cv.minAreaRect(c)[1][1]) >10000]
+  rects = [cv.minAreaRect(c) for c in filter]
+
+  image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+  mindiff = 180
+  pair = (None, None)
+  angle = 0
+
+  #print(rects[0][0])
+
+  for i in range(len(rects)):
+    for j in range(i+1, len(rects)):
+
+      ri = rects[i][2]
+      rj = rects[j][2]
+
+      wi, hi = rects[i][1]
+      wj, hj = rects[j][1]
+
+      if wi < hi:
+        ri = ri + 90
+      if wj < hj:
+        rj = rj + 90
+
+      linediff = abs(ri - rj)
+      linediff = min(linediff, 180 - linediff)
+      dist = (rects[i][0][0] - rects[j][0][0])
+
+      if linediff < mindiff and dist > 100:
+        #print(dist)
+        mindiff = linediff
+        pair = (filter[i], filter[j])
+        rectx = int((rects[j][0][0] + rects[i][0][0])/2)
+        recty = int((rects[j][0][1] + rects[i][0][1])/2)
+        angle = (rj + ri)/2
+
+   
+  if pair[0] is not None:
+    cv.circle(image, (rectx, recty), 10, (255,0,0),2)
+
+    mapp = np.interp(int(angle), [0, 180], [0, 640])
+    
+    if rectx in range(290, 350):
+      ser.write(bytes(str(int(mapp)) + '\n', encoding="utf-8"))
+      cv.circle(image, (int(mapp), recty), 10, (255,255,0),2)
+    else:
+     ser.write(bytes(str(int(rectx)) + '\n', encoding="utf-8"))
+     #print(rectx)
+
+    if ser.in_waiting > 0:          
+      line = ser.readline().decode('utf-8').rstrip()
+      print("line", line)
+
+    for item in pair:
+      box = cv.minAreaRect(item)
+      box = cv.boxPoints(box)
+      box = np.int_(box)
+      cv.drawContours(image, [box], 0, (255, 0, 0), 2)      
+      rows, cols = (frame.shape[:2])
+
+      [vx, vy, x, y,] = cv.fitLine(item, cv.DIST_L2,0,0.01,0,0.01)
+      if vx > 0:
+        lefty = int((-x*vy/vx)+y)
+        righty = int(((cols-x)*vy/vx)+y)
+        cv.line(image,(cols-1,righty),(0,lefty),(0,255,0),2)
+  elif rects:
+    size = 0
+    r = 0
+    for box in rects:
+      if (box[1][0]*box[1][1]) > size and (box[1][0]*box[1][1]) < 30000:
+        size = (box[1][0]*box[1][1])
+        r = box
+    #print(size)  
+    if r:
+      box = cv.boxPoints(r)
+      box = np.int_(box)
+      cv.drawContours(image, [box], 0, (0, 255, 0), 2)       
+      rx = box[0][0]                                                                                                          
+      w, h = box[1]
+      if rx > 320:
+        ser.write(bytes(str(100) + '\n', encoding="utf-8"))
+      else:
+        ser.write(bytes(str(int(540)) + '\n', encoding="utf-8"))
+
+    #if w < h:
+       # r = r + 90
+    #rx = rects[0][0][0]
+
+
+
+
+    #if rx > 320:
+     # mapp = np.int0], [0, 320])   
+     # ser.write(bytes(str(int(mapp)) + '\n', encoding="utf-8"))
+   # else:
+     # mapp = np.interp(r, [0, 180], [320, 640])   
+     # ser.write(bytes(str(int(mapp)) + '\n', encoding="utf-8"))
+
+  
+  cv.imshow('imageC', image)
+  #hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+  out.write(image)
+  key = cv.waitKey(1)
+  if key == 27: 
+
+    break
+
+cam.release()
+out.release()
+cv.destroyAllWindows()
